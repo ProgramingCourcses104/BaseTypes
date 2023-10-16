61994a55f01d4176b1d86e4e1c4f87a1dd88a2ec
prizrak107 <prizrak107@DESKTOP-AC7E7UT>
2023-10-16 15:50:42 +0300

    Add project files.

---
+++ BaseTypes/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BaseTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write anything: ");
+
+            string inputString = Console.ReadLine();
+
+            if(double.TryParse(inputString, out double doubleResult))
+            {
+                Console.WriteLine("input string : '" + doubleResult + "' is double");
+            }
+            else
+            {
+                Console.WriteLine($"input string : '{doubleResult}' is not double");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CustomTypes/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CustomTypes
+{
+    internal class Program
+    {
+        class User
+        {
+            public string Name;
+            public int Age;
+
+            public User() { }
+            
+            public User(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+
+        struct StuctUser
+        {
+            public string Name;
+            public int Age;
+
+            public StuctUser(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            Console.WriteLine("Write name: ");
+            string nameInput = Console.ReadLine();
+
+            if (nameInput != null || nameInput != "") {
+                user.Name = nameInput;
+            }
+
+            Console.WriteLine("Write age: ");
+            string ageInput = Console.ReadLine();
+
+            if (int.TryParse(ageInput, out int ageParsed))
+            {
+                user.Age = ageParsed;
+            }
+            else
+            {
+                Console.WriteLine($"input age : '{ageInput}' is not correct");
+            }
+
+            Console.WriteLine($"User name: {user}\n User age: {user.Age}");
+
+            Console.ReadKey();
+        }
+    }
+}
