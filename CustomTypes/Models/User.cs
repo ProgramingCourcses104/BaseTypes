0d2af490f094004663855bbe5d314c3128a32ce1
valentyn yarema <valik9988@outlook.com>
2023-10-16 16:09:40 +0300

    refactor split the code

---
+++ CustomTypes/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomTypes.Models
+{
+    public class User
+    {
+        public string Name;
+        public int Age;
+
+        public User() { }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
